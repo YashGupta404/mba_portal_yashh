6c5c71706d0110e3fa4eafb98d3b47b30b421ceb
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-12-12 18:22:34 +0530

    Inquiry dialogue box and inquiry form

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -1,7 +1,10 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { ChevronRight, Sparkles } from "lucide-react"
+import { ChevronRight, Sparkles, CircleX } from "lucide-react"
+import clsx from "clsx"
+import { Form } from "../ui/form"
+
 
 export default function HeroSection() {
   const [imageIndex, setImageIndex] = useState(0)
@@ -29,15 +32,27 @@ export default function HeroSection() {
     setParticles(generatedParticles)
   }, [])
 
+
+  const [style, setstyle] = useState('hidden');
+  const handleclick = () => {
+    if (style === "block") {
+      document.body.style.overflow = "auto";
+      setstyle("hidden");
+    }
+    else {
+      document.body.style.overflow = "hidden";
+      setstyle("block")
+    }
+  }
+  const formfield="h-10 rounded-[4px] hover:border-blue-800 bg-accent/10 hover:border-[2px] border-box py-1 px-3";
   return (
     <section className="relative w-full h-screen overflow-hidden">
       {/* Animated Background Images with Zoom Effect */}
       {images.map((img, idx) => (
         <div
           key={idx}
-          className={`absolute inset-0 transition-all duration-1500 ${
-            idx === imageIndex ? "opacity-100 scale-105" : "opacity-0 scale-100"
-          }`}
+          className={`absolute inset-0 transition-all duration-1500 ${idx === imageIndex ? "opacity-100 scale-105" : "opacity-0 scale-100"
+            }`}
           style={{
             backgroundImage: `url(${img})`,
             backgroundSize: "cover",
@@ -70,8 +85,43 @@ export default function HeroSection() {
           />
         ))}
       </div>
+      {/*form*/}
+      <div className={clsx("w-screen h-screen top-0  fixed z-90 bg-black/50", style)}>
+        <div id="homeenquiryform" className={clsx("flex flex-col items-end border-box py-5 px-8 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100 w-[50vw] h-[90vh]  bg-zinc-50 transition-all duration-300 transform ease-out rounded-[10px] ", style === "block" ? "opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-95 pointer-events-none")}>
+          <button type="button" onClick={handleclick} className="w-auto  ">
+            <CircleX size={40} className="text-accent" strokeWidth={2.5} />
+          </button>
+          <div className="w-full h-full flex flex-col justify-around">
+            <div>
+              <p className="text-[3rem] text-center font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent/50 to-blue-900 leading-tight" >MBA Course Enquiry</p>
+              <p className="text-[0.8rem] text-center font-bold">Having enquiry regarding our MBA course?? Feel free to ask us through our portal</p>
+            </div>
+            <form className="grid grid-cols-2 grid-rows-6 gap-7  w-full h-auto">
+              <input placeholder="Enter Name e.g. xxx xx" className={clsx(formfield)}/>
+              <input placeholder="Enter Email Id e.g. xxxx@gmail.com" className={clsx(formfield)}/>
+              <input placeholder="Enter Mobile Number e.g. xxxxxxxxxx" className={clsx(formfield)}/>
+              <select className={clsx(formfield)}>
+                <option value="">Enquiry Type</option>
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+              </select>
+              <textarea  placeholder="State your Enquiry..." className={clsx(formfield,"col-span-2 row-span-3 h-full resize-none ")}>5</textarea>
+              <button className={clsx("col-span-2 text-white bg-gradient-to-b from-blue-800 to-blue-500")}>Submit your query</button>
+            </form>
 
-      <div className="absolute top-20 right-10 w-32 h-32 rounded-full bg-accent/10 blur-3xl animate-float" />
+          </div>
+        </div>
+      </div>
+      {/*enquiry box*/}
+      <div className="absolute z-3 bottom-10 right-10 w-60 h-auto rounded-bl-[20px] rounded-tr-[20px] bg-white/40 backdrop-blur-md border border-white/60 
+            shadow-lg  p-6 animate-float p-1 border-box flex flex-col justify-between  gap-4" >
+        <p className=" text-white drop-shadow-[3px_3px_4px_rgba(0,0,0,0.9)] text-center leading-tight">Want to know more About MBA program?</p>
+        <button type="button" onClick={handleclick} className="py-1 hover:shadow-[3px_3px_4px_rgba(0,0,0,0.3)] rounded-l-full rounded-r-full border border-[2px] bg-black/50 hover:bg-black/45 text-primary-foreground transition-all transition-100 active:scale-98 hover:scale-103">
+          Enquire Here
+        </button>
+      </div>
       <div className="absolute bottom-32 left-10 w-40 h-40 rounded-full bg-secondary/10 blur-3xl animate-float-delayed" />
       <div className="absolute top-1/2 right-1/4 w-24 h-24 rounded-full bg-primary/5 blur-2xl animate-float-slow" />
 
@@ -144,6 +194,7 @@ export default function HeroSection() {
                 className="w-1.5 h-3 bg-white/50 rounded-full animate-slide-in-up"
                 style={{ animationDelay: "0s" }}
               />
+
             </div>
           </div>
         </div>
