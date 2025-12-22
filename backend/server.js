e9551902e18b1a9f51cf3abac43e8e832996e475
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-12-22 17:42:18 +0530

    added put cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 // Middleware
 app.use(cors({
         origin:['http://localhost:3000',"http://localhost:8080"],
-        methods:['POST',"GET"],
+        methods:['POST',"GET","PUT"],
         credentials:true
     })
 );
