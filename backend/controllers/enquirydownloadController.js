a67c274cb513b8a3b6d0034f14b512a5b0edfc00
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-12-23 12:21:37 +0530

    completed export enquireis download backend as xlsx file

--- backend/controllers/enquirydownloadController.js
+++ backend/controllers/enquirydownloadController.js
@@ -5,16 +5,38 @@ import xlsx from "xlsx";
 
 export const downloadenquiries = async (req, res) => {
   try {
-    const enquiries = await enquirymodel
-      .find({}, { name: 1,email:1,mobile:1, subject: 1, message: 1, _id: 0,status:0,priority:0,date:1})//specifying columns to include
-      .lean(); //converts mongodb docuements to plain js objects
+    const enquiries = await enquirymodel.find(
+      {},
+      {
+        name: 1,
+        email: 1,
+        mobile: 1,
+        subject: 1,
+        message: 1,
+        date: 1,
+        _id: 0,
+      }
+    ).lean(); //converts mongodb docuements to plain js objects
     if (enquiries.length === 0) {
       return res.json({
         success: false,
         message: "No enquiries to download",
       });
     }
-    const worksheet = xlsx.utils.json_to_sheet(enquiries); //Each object becomes a row, each key becomes a column.
+
+    //removes the risk of defined data values 
+    const cleanData = enquiries.map(e => ({
+      Name: e.name || "",
+      Email: e.email || "",
+      Mobile: e.mobile || "",
+      Subject: e.subject || "",
+      Message: e.message || "",
+      Date: e.date ? new Date(e.date).toLocaleString() : "",
+    }));
+
+
+    const worksheet = xlsx.utils.json_to_sheet(cleanData); //Each object becomes a row, each key becomes a column.
+    
     const workbook = xlsx.utils.book_new(); //entire workbook
     xlsx.utils.book_append_sheet(workbook, worksheet, "Enquiries"); //append worksheet to workbook with the name "Enquiries"
     const buffer = xlsx.write(workbook, {
